d57b8f628df4634a30374c589e45ea94c295582b
Wilson <wilsonbm11@gmail.com>
2024-06-21 16:40:55 -0600

    Codigo base con chatbot implementado

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Main from './pages/Main/Main';
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Main />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
